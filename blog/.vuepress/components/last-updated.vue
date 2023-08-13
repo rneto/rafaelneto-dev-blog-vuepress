027f836c13a3c07450e0a42086ad49fec9ea310b
rneto <rrnoak@gmail.com>
2023-08-13 12:25:11 +0200

    Review data visualization in last-update component

--- blog/.vuepress/components/last-updated.vue
+++ blog/.vuepress/components/last-updated.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="last-updated hidden">
+  <div class="last-updated">
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
@@ -21,20 +21,20 @@
       }
     },
     mounted: function() {
-      this.appendToHeader()
+      // this.appendToHeader()
     },
     methods: {
-      appendToHeader: function() {
-        setTimeout(() => {
-          let headerReference = document.querySelector('.post-meta-date');
-          document.querySelector('.last-updated:not(.hidden)')?.remove();
-          let lastUpdatedReference = document.querySelector('.last-updated.hidden');
-          if (headerReference && lastUpdatedReference) {
-            lastUpdatedReference.classList.remove('hidden');
-            headerReference.after(lastUpdatedReference);
-          }
-        }, 100);
-      }
+      // appendToHeader: function() {
+      //   setTimeout(() => {
+      //     let headerReference = document.querySelector('.post-meta-date');
+      //     document.querySelector('.last-updated:not(.hidden)')?.remove();
+      //     let lastUpdatedReference = document.querySelector('.last-updated.hidden');
+      //     if (headerReference && lastUpdatedReference) {
+      //       lastUpdatedReference.classList.remove('hidden');
+      //       headerReference.after(lastUpdatedReference);
+      //     }
+      //   }, 100);
+      // }
     }
   }
 </script>
@@ -44,6 +44,7 @@
     display: inline-flex;
     line-height: 12px;
     font-size: 12px;
-    margin-right: 20px;
+    margin-top: 20px;
+    /* margin-right: 20px; */
   }
 </style>

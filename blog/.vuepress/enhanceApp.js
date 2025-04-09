a200fdab14394508f188ec310389c58f47455eb3
rneto <rrnoak@gmail.com>
2025-04-09 07:53:33 +0200

    Add dynamic title update for Twitter share buttons to reflect branding change to X

--- blog/.vuepress/enhanceApp.js
+++ blog/.vuepress/enhanceApp.js
@@ -8,7 +8,23 @@ export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  siteData, // site metadata
 }) => {
   // ...apply enhancements to the app
-}
+  if (typeof window !== "undefined") {
+    const waitForAllTwitterButtons = () => {
+      const buttons = document.querySelectorAll(
+        '.social-share-btn[data-link="#share-twitter"]'
+      );
+      if (buttons.length > 0) {
+        buttons.forEach((btn) => {
+          btn.title = "Share on X";
+        });
+      } else {
+        requestAnimationFrame(waitForAllTwitterButtons);
+      }
+    };
+
+    waitForAllTwitterButtons();
+  }
+};
